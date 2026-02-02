67c82d55111241a54ff28d0c85ed98795e5efa9a
Pratap Padhy <pratappadhy2005@gmail.com>
2026-02-02 19:24:18 +1100

    -Added root route logic in server.js

--- 12-End-ToEnd/18. bookstore-api/server.js
+++ 12-End-ToEnd/18. bookstore-api/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectToMongoDB = require('./database/db');
+const bookRoutes = require('./routes/book-routes');
 
 dotenv.config();
 
 connectToMongoDB();
 
 const app = express();
+
+//parse request body as a json
+app.use(express.json());
+
+//use book routes
+app.use('/api/books', bookRoutes);
 
 const port = process.env.PORT || 3000;
 
