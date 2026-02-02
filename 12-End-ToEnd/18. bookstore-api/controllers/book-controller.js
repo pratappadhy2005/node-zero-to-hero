bd0bd1dd202ab22ff7c80e2c5e3d96c6498eda7e
Pratap Padhy <pratappadhy2005@gmail.com>
2026-02-02 21:23:50 +1100

    -Updated controller methods with proper http sttaus code

--- 12-End-ToEnd/18. bookstore-api/controllers/book-controller.js
+++ 12-End-ToEnd/18. bookstore-api/controllers/book-controller.js
@@ -3,6 +3,9 @@ const Book = require("../models/Book");
 const getAllBooks = async (req, res) => {
     try {
         const books = await Book.find();
+        if (!books) {
+            return res.status(404).json({ message: "No books found" });
+        }
         res.status(200).json(books);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -12,6 +15,9 @@ const getAllBooks = async (req, res) => {
 const getSingleBookById = async (req, res) => {
     try {
         const book = await Book.findById(req.params.id);
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });
+        }
         res.status(200).json(book);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -23,6 +29,9 @@ const updateBook = async (req, res) => {
         const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
         });
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });
+        }
         res.status(200).json(book);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -32,6 +41,9 @@ const updateBook = async (req, res) => {
 const deleteBook = async (req, res) => {
     try {
         const book = await Book.findByIdAndDelete(req.params.id);
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });
+        }
         res.status(200).json(book);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -40,7 +52,14 @@ const deleteBook = async (req, res) => {
 
 const addNewBook = async (req, res) => {
     try {
+        //validate request body
+        if (!req.body.title || !req.body.author || !req.body.year) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
         const book = await Book.create(req.body);
+        if (!book) {
+            return res.status(404).json({ message: "Book creation failed" });
+        }
         res.status(201).json(book);
     } catch (error) {
         res.status(500).json({ message: error.message });
