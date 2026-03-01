0b643ac92f93f4dafd831aec3449f651a9739235
Pratap Padhy <pratappadhy2005@gmail.com>
2026-03-01 22:00:14 +1100

    -Async programming started-2

--- 12-End-ToEnd/11. callbacks vs async await vs promises/async.js
+++ 12-End-ToEnd/11. callbacks vs async await vs promises/async.js
@@ -1,12 +1,10 @@
 console.log("start operation");
 
 function sleep(miliseconds) {
-    let start = new Date().getTime();
-    console.log("Operation started at " + start);
-    while (new Date().getTime() - start < miliseconds) {
-        console.log("Operation is in progress" + (new Date().getTime() - start));
-    }
-    console.log("Operation completed at " + new Date().getTime());
+    console.log("Operation is running");
+    setTimeout(() => {
+        console.log("Operation is done");
+    }, miliseconds);
 }
 
 sleep(2000);
