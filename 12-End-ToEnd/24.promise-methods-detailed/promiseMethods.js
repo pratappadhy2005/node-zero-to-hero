a1b1ea721ac5e4a92d0be6b725f1516ded9b390f
Pratap Padhy <pratappadhy2005@gmail.com>
2026-03-25 22:43:20 +1100

    -Added prmoise all example

---
+++ 12-End-ToEnd/24.promise-methods-detailed/promiseMethods.js
@@ -0,0 +1,21 @@
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Promise 1 resolved');
+    }, 2000);
+})
+
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Promise 2 resolved');
+    }, 2000);
+})
+
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Promise 3 resolved');
+    }, 2000);
+})
+
+Promise.all([promise1, promise2, promise3])
+    .then(results => console.log(results))
+    .catch(err => console.log('Promise all rejected:', err));
