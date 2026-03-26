18e7a8facc89f882f7b24a5c7324873745d2e8b1
Pratap Padhy <pratappadhy2005@gmail.com>
2026-03-26 22:19:56 +1100

    -Added custom logging middleware

--- 13-MicroServices-Advanced/express-concepts/server.js
+++ 13-MicroServices-Advanced/express-concepts/server.js
@@ -2,15 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const { corsOptions } = require('./config/corsConfig');
 const cors = require('cors');
+const requestLogger = require('./middleware/errorHandler');
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// request logger middleware
+app.use(requestLogger);
+
+// add timestamp middleware
+app.use(requestLogger.addTimeStamp);
+
 app.use(cors(corsOptions));
 
 // express json middleware
 app.use(express.json());
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
