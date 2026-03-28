8296bd0402140d793992114bb6135b02e8aa6cce
Pratap Padhy <pratappadhy2005@gmail.com>
2026-03-28 22:11:40 +1100

    -Added increment counter with redis and delete key

--- 13-MicroServices-Advanced/redis/server.js
+++ 13-MicroServices-Advanced/redis/server.js
@@ -18,12 +18,31 @@ client.on('error', (err) => {
 async function testRedis() {
     try {
         await client.connect();
+
+        // set key
         await client.set('test', 'hello world');
         console.log('Key set successfully');
         const value = await client.get('test');
         console.log('Value:', value);
+
+        //delete key
+        await client.del('test');
+        console.log('Key deleted successfully');
+
+        //check if key exists
+        const updatedValue = await client.get('test');
+        console.log('Updated Value:', updatedValue);
+
+        //increment count
+        await client.incr('count');
+        console.log('Count incremented successfully');
+        await client.set('count', 0);
+        console.log('Count set successfully');
+        const count = await client.incr('count');
+        console.log('Count:', count);
+
     } catch (err) {
-        console.error('Error setting key:', err);
+        console.error('Error deleting key:', err);
     } finally {
         await client.quit();
     }
