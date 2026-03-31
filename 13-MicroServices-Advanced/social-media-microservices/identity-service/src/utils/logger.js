ca36dad3279d9d9283a23217ec65017e79024f41
Pratap Padhy <pratappadhy2005@gmail.com>
2026-03-31 17:36:08 +1100

    -Added logging configuration using winston

---
+++ 13-MicroServices-Advanced/social-media-microservices/identity-service/src/utils/logger.js
@@ -0,0 +1,39 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
+    format: winston.format.combine(
+        winston.format.errors({ stack: true }),
+        winston.format.splat(),
+        winston.format.timestamp(),
+        winston.format.colorize(),
+        winston.format.json()
+    ),
+    defaultMeta: {
+        service: 'identity-service',
+    },
+    transports: [
+        new winston.transports.Console(
+            {
+                format: winston.format.combine(
+                    winston.format.combine(),
+                    winston.format.timestamp(),
+                    winston.format.simple()
+                )
+            }
+        ),
+        new winston.transports.File(
+            {
+                filename: 'error.log',
+                level: 'error'
+            }
+        ),
+        new winston.transports.File(
+            {
+                filename: 'combined.log'
+            }
+        ),
+    ]
+});
+
+module.exports = logger;
