e60093af585e1f86b142c6b57574fd0d4e73ad44
Pratap Padhy <pratappadhy2005@gmail.com>
2026-04-06 09:59:47 +1000

    -Added API Gateway micro service which acts as a proxy

---
+++ 13-MicroServices-Advanced/social-media-microservices/api-gateway/src/server.js
@@ -0,0 +1,82 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const Redis = require('ioredis');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const { RedisStore } = require('rate-limit-redis');
+const app = express();
+const logger = require('./utils/logger');
+const proxy = require('express-http-proxy');
+const errorHandler = require('./middleware/errorHandler');
+
+const PORT = process.env.PORT || 3000;
+
+//connect to the redis
+const redisClient = new Redis(process.env.REDIS_URL);
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+
+//rate limit
+const endpointRateLimit = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+    handler: (req, res) => {
+        logger.warn(`Sensitive endpoints too many requests from ${req.ip}`);
+        res.status(429).json({
+            message: 'Too many requests from this IP',
+            success: false,
+        });
+    },
+    store: new RedisStore({
+        sendCommand: (...args) => redisClient.call(...args),
+    }),
+});
+
+app.use(endpointRateLimit);
+
+// logging
+app.use((req, res, next) => {
+    logger.info(`Request received at ${new Date().toISOString()} with ${req.method} ${req.url} and body ${JSON.stringify(req.body)}`);
+    next();
+});
+
+//proxy to redirect requests to identity service
+const proxyOptions = {
+    proxyReqPathResolver: (req) => {
+        return req.originalUrl.replace(/^\/v1/, '/api');
+    },
+    proxyErrorHandler: (err, res, next) => {
+        logger.error(`Proxy error: ${err.message}`);
+        res.status(500).json({
+            message: 'Internal Server Error',
+            error: err.message,
+        });
+    },
+};
+
+//setting proxy for our identity service
+app.use('/v1/auth', proxy(process.env.IDENTITY_SERVICE_URL, {
+    ...proxyOptions,
+    proxyReqOptDecorator: (proxyReqOpts) => {
+        proxyReqOpts.headers['Content-Type'] = 'application/json';
+        return proxyReqOpts;
+    },
+    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+        logger.info(`Proxy response status: ${proxyRes.statusCode}`);
+        return proxyResData;
+    },
+}));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    logger.info(`API Gateway is running on port ${PORT}`);
+    logger.info(`Identity Service URL: ${process.env.IDENTITY_SERVICE_URL}`);
+    logger.info(`Redis URL: ${process.env.REDIS_URL}`);
+});
